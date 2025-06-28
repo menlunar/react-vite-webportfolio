3750b0285c9771bb751d6de37c2373c41cb37354
Menric <menlunar@gmail.com>
2025-06-28 14:13:45 +0800

    cleanups, basic skeleton made

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,59 +12,42 @@ function App() {
     <>
       <Navbar />
       <div className="content">
-        <h1>Welcome motherfucker</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque excepturi exercitationem sit ea enim voluptatum placeat nostrum consequatur blanditiis, odit aliquid eaque quam earum consequuntur, eius officia modi unde! Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto ad dolores, corporis earum nobis! Eum numquam amet quis ducimus perferendis obcaecati accusantium voluptates, quas, id optio repellendus atque? Cum?
-          Dignissimos culpa, minima nam ex, unde ipsum dolore labore sint dicta est asperiores, veniam accusamus itaque repellat eius maiores nulla quibusdam deserunt dolores odio sed. Cum perspiciatis ducimus et libero.
-          Fugit velit repellendus laborum a eligendi neque, nam perspiciatis corporis laudantium? Unde nostrum, blanditiis quidem fuga sequi porro nihil saepe hic velit consequatur vel voluptates in officiis voluptatum, molestias cupiditate?
-          Molestiae animi ipsa aut dolor, libero harum quas quia eos nihil dolorem delectus! Quia doloribus, est quibusdam cumque id aperiam eius, quaerat accusantium suscipit impedit totam asperiores. Iusto, animi inventore.
-          Veniam reprehenderit ducimus impedit consequatur, ut maiores mollitia, eaque quo repellendus dolores delectus non possimus alias! Aperiam tempore deserunt facere quod omnis ex veritatis, eligendi modi hic, nobis ab in?
-          In nemo maiores saepe ratione autem exercitationem praesentium, vel ex quidem enim est voluptate, ad, nobis reprehenderit porro tenetur. Eveniet explicabo perferendis itaque adipisci quaerat ad sequi at dolorem aliquid?
-          Nemo provident excepturi sunt quasi nulla quo earum recusandae fuga, laborum, optio cumque corporis cum eos esse quisquam temporibus non ea nesciunt et voluptates itaque. Molestias deserunt quasi unde officiis.
-          Dicta maxime pariatur corrupti, adipisci dolorum esse officiis fuga maiores facilis ea nam deleniti, natus iste, est quas. Earum numquam commodi ea illo delectus perspiciatis vitae mollitia nostrum dolor id!
-          Tempore eligendi aliquam, tenetur libero sequi at suscipit optio maiores ducimus dolores id harum perferendis, eum ab quam rem similique doloribus cumque enim voluptatem exercitationem dolor. Consequuntur maxime corporis laborum.
-          Quos mollitia aliquid facilis eaque eveniet quae deserunt id fugit ipsam illo voluptatem culpa necessitatibus, neque repellendus a veritatis adipisci reprehenderit rem modi doloribus enim facere. Officia dignissimos ducimus aut?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores quis id eligendi aperiam dolor ullam quod officia, nulla perferendis voluptate ipsum quos quam dicta reprehenderit, architecto, aspernatur labore. Modi, quia. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam doloremque excepturi recusandae dolores totam nemo minima, amet qui omnis ea inventore aut quod illum earum alias facilis corporis nam?
-          Distinctio repellendus, animi assumenda sit, exercitationem dolores nostrum fuga accusamus dolor, dolorum odio iure. Deleniti, excepturi? Voluptatem, facere! Modi dolorem cumque ad est quos. Excepturi quidem eligendi earum aut voluptatem.
-          Dolorem molestiae ad pariatur a laudantium rerum impedit harum veritatis! Sit reprehenderit vel dicta est alias quibusdam unde quis voluptates. Repudiandae pariatur fuga totam eius tempore ut architecto distinctio rem?
-          Neque tempora nihil tenetur recusandae alias voluptates est dolor voluptatem, animi numquam, suscipit et qui sit deserunt placeat ratione nostrum reiciendis ea fugiat quas explicabo? Ea debitis tenetur molestias sunt?
-          Ducimus beatae, totam nobis enim numquam nisi quidem, distinctio explicabo autem error nostrum sapiente. Nisi, deleniti laborum. Vero corrupti eligendi dicta ex alias sint mollitia qui, aliquid, tempora enim excepturi?
-          Quo tempora beatae voluptate omnis quasi nulla possimus tempore porro voluptatem magnam maxime cupiditate ex dolorum, repellat voluptatibus reprehenderit et vitae veniam debitis hic quod. Consequatur unde enim natus sed?
-          Reprehenderit ipsam unde maxime nulla magnam ullam tempore numquam nostrum. Molestiae incidunt soluta quis architecto delectus sed, eos qui corrupti. Asperiores ex dolores nihil laudantium eum rerum quidem laboriosam numquam?
-          In, quos pariatur nemo explicabo soluta unde amet expedita quasi facere consequatur, iste praesentium. Ducimus quas, facilis est, officiis, quos qui nemo placeat vel nobis odio quia dolorem deserunt maxime!
-          Iure, animi sequi consequatur est earum corporis similique velit labore debitis ipsum aut dicta reprehenderit. Aliquam nostrum reiciendis, porro cupiditate totam quas possimus quos nesciunt, nemo doloribus, quaerat earum vel?
-          Rem beatae voluptas corrupti ratione dolorem. Id quam voluptatum cumque voluptatibus eveniet blanditiis laborum ex vitae, soluta voluptates delectus, tempora, facilis exercitationem! Vitae et unde facilis repellendus temporibus eius omnis?
-          Quis alias nulla itaque repudiandae commodi, minus quae perspiciatis sunt consequatur quasi quam dolorum modi eveniet sint placeat aut sed totam architecto ab inventore fugiat maiores. Veniam atque modi cum.
-          Maiores dolorem quo consectetur voluptatibus ipsam! Accusamus dolores quia quas praesentium at. Doloribus ab ex, maiores labore blanditiis adipisci accusantium aspernatur! Nam earum quasi repellat pariatur! Libero labore ipsum ad.
-          Saepe harum ad est aut repellendus labore voluptates porro rem nostrum quos aliquam sapiente, accusantium quasi voluptas amet maiores alias impedit aperiam laboriosam. Dicta, tempore. Voluptatem quas eum atque sunt!
-          Facere similique ab quam provident doloribus recusandae, necessitatibus nesciunt eum accusantium, fuga excepturi sunt culpa amet qui, voluptatibus officia repellendus labore? Adipisci odio dolorem mollitia consequatur quisquam necessitatibus accusantium! Quod.
-          Itaque quia vero voluptas veniam assumenda aliquid laudantium sint in ducimus impedit exercitationem, labore magnam amet omnis. Dolorem omnis rem nesciunt consequuntur! Molestiae nulla, nihil odio quas tenetur tempore eos.
-          Odio architecto facere quas, veniam voluptate porro voluptatum similique maiores cumque inventore in, distinctio asperiores possimus impedit quibusdam unde doloremque dignissimos. Sit quo, eveniet minus aliquid dignissimos molestias tenetur magnam.
-          Alias magnam, debitis velit, nostrum omnis quidem eos architecto mollitia hic numquam amet est, veniam deleniti facilis quasi doloremque soluta quisquam minus magni? Voluptate suscipit sapiente vitae labore facilis exercitationem.
-          Rerum, officiis sit quas officia aperiam consequatur esse dolor iste neque nesciunt voluptatem dolorem omnis, consequuntur, quis harum assumenda temporibus tenetur tempora optio maxime voluptates quasi quaerat eum? Unde, illo.
-          Sequi placeat blanditiis suscipit facilis molestiae maxime amet a soluta voluptatem quia quibusdam temporibus alias ducimus, cum ullam? Nihil provident autem accusantium velit quisquam quibusdam doloribus at. Nostrum, reiciendis eligendi.
-          Hic reiciendis reprehenderit illo ratione et earum quam dolores neque, voluptate libero tempore quia iure ut. Sequi excepturi labore dicta, quis fugit, iusto tenetur quam, aliquam esse ut architecto necessitatibus.</p>
-      </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+        <section id="home">
+          <h1>Welcome motherfucker</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quisquam hic esse quis fuga. Pariatur necessitatibus odio reiciendis in aliquam quam ab nam dolores animi, nisi corrupti? Odio, iusto reprehenderit!</p>
+        </section>
 
+        <section id="projects">
+          <h2>Projects</h2>
+          <p>List your projects here.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam veniam et consequatur harum dicta asperiores accusamus error totam perferendis exercitationem, beatae voluptatibus. Sed nulla fuga blanditiis quo, et alias.
+            Maxime doloremque eveniet et magnam deserunt laborum illo sed unde iure, reiciendis molestiae, adipisci explicabo? Quia, ab dolorum molestiae assumenda quo debitis voluptatem totam dolore hic alias doloribus expedita tempora.
+            Necessitatibus recusandae, aut ab nisi dolorem nostrum dolores est numquam, libero asperiores expedita, sunt placeat ipsam aspernatur a consectetur quo inventore porro doloremque at eos? Pariatur eum alias quia dicta!
+            A modi temporibus expedita ad nihil quos deserunt quae iure? Voluptates adipisci a, tempora ipsam laudantium blanditiis quo magni, consectetur optio tenetur quibusdam modi facilis perferendis eligendi facere labore ullam.
+            Debitis inventore, eligendi sunt eaque est nemo dolorum, deleniti sapiente quas in itaque magni tempora accusantium nobis, culpa a sit atque iste voluptatibus distinctio illum. Repudiandae sed quis laborum temporibus.</p>
+        </section>
 
+        <section id="about">
+          <h2>About Me</h2>
+          <p>This is the about section.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quis obcaecati. Recusandae quia architecto fugiat natus praesentium, eligendi quam reprehenderit illo ducimus, atque nulla ut obcaecati dignissimos accusantium, quae tempore!
+            Temporibus maxime reiciendis, delectus voluptatum tenetur illum ab vel, veniam quibusdam ad cupiditate ipsa mollitia suscipit possimus magnam velit saepe, est eaque nemo officia necessitatibus explicabo id. Eos, odit eveniet?
+            Dolores quas perspiciatis, natus voluptatem hic dolorum. Aliquid numquam ut alias laboriosam asperiores nulla animi illum! Quos ipsa animi eos beatae unde aliquid optio minus, cumque porro ab saepe nesciunt.
+            Dignissimos deserunt ex atque reprehenderit delectus? Quaerat voluptatem debitis eligendi! Deserunt exercitationem quisquam amet eos illum voluptatibus neque non adipisci, nobis sunt necessitatibus mollitia ipsum, eligendi dolorem corporis velit vitae!
+            Recusandae, assumenda perspiciatis exercitationem neque nobis voluptate omnis sed rem vero totam in pariatur cupiditate nam explicabo asperiores ad odio cum, doloribus quos iure. Eum provident aperiam minima. Perspiciatis, dicta.
+            Illum ducimus rerum doloremque repudiandae minima libero quae aliquam ex aliquid? Reiciendis ipsa ex officia nulla labore enim placeat quisquam facere eos voluptatum, quos iusto dolores necessitatibus eveniet harum optio!
+            Totam rem quibusdam fuga iste minus impedit, optio veritatis ab ipsa. Quo dolorum suscipit laudantium? Nulla placeat odit quibusdam neque dolores aspernatur quisquam corrupti, voluptatem doloribus magni eum quam hic.
+            Adipisci, est reprehenderit praesentium in dicta quaerat illo porro voluptas voluptates odio at cumque aperiam quod fuga. Aliquam suscipit dolorem nisi modi exercitationem! Reprehenderit sequi aperiam suscipit maxime nobis quas!
+            Facere laborum et error fuga, quaerat vitae inventore. Laborum, facere? Totam eaque consequuntur dolore ea ipsam, reprehenderit ex officiis tenetur voluptate perferendis! Cum quisquam aperiam, consequuntur iste eius suscipit quasi.
+            Repudiandae tempore vel maiores unde facilis sunt magni impedit praesentium perspiciatis dolor dignissimos, incidunt mollitia harum necessitatibus ratione iure nihil laborum architecto nostrum labore hic eaque commodi ullam. Cum, provident.</p>
+        </section>
+
+        <section id="contact">
+          <h2>Contact</h2>
+          <p>Here’s how to reach me.</p>
+        </section>
+      </div>
+      <Footer />
     </>
   )
 }
@@ -75,13 +58,21 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="logo">Menric G. Lunar</div>
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
     </header>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Menric G. Lunar. All rights reserved.</p>
+    </footer>
   );
 };
 
